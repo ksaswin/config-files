75550b332ba03b84e4a3c0471a9dadd04c0c0d38
Aswin-Shaji <aswin2000.ak@gmail.com>
2024-03-27 16:59:03 +0530

    Add new notify_installation function to helpers

--- installer-scripts/helpers.sh
+++ installer-scripts/helpers.sh
@@ -1,15 +1,23 @@
 #!/usr/bin/env bash
 
 
+notify_installation() {
+  app=$1
+
+  echo "Installing $app..."
+}
+
+
 run_installation () {
   installer=$1
   app_name=$2
   app_display_name=$3
 
-  echo "Installing $app_display_name..."
+  notify_installation "$app_display_name"
   $installer search $app_name # FIXME: Only performing search
   # $installer install $app_name
 }
+
 
 # This function can be accessed from other scripts
 # If the application is a cask, the `app_name` expects value as "--cask app_name"
