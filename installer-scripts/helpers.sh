ab5aa8d97575b47c6b455a7820966c44460a33af
Aswin-Shaji <aswin2000.ak@gmail.com>
2024-04-01 10:22:06 +0530

    Update sed commands for linux and mac machines

--- installer-scripts/helpers.sh
+++ installer-scripts/helpers.sh
@@ -81,6 +81,13 @@ clone_repo_to_dir () {
 oh_my_zsh () {
   home=$1
 
+  uname_out="$(uname -s)"
+
+  case "${uname_out}" in
+      Linux*)     machine=Linux;;
+      Darwin*)    machine=Mac;;
+      *)          machine="UNKNOWN"
+  esac
 
   zshrc_filepath="$home/.zshrc"
   echo "Backing up the ~/.zshrc file to ~/.zshrc.bk"
@@ -92,15 +99,23 @@ oh_my_zsh () {
   zsh_custom="$home/.oh-my-zsh/custom"
   clone_repo_to_dir "PowerLevel10k" "https://github.com/romkatv/powerlevel10k.git" "$zsh_custom/themes/powerlevel10k"
   echo "Updating the ZSH_THEME in .zshrc to use powerlevel10k"
-  # sed command with -i and -e flags works on Mac
   # Intended purpose: Find and replace ZSH_THEME value and write changes to .zshrc
-  sed -i -e 's/^ZSH_THEME=".*"/ZSH_THEME="powerlevel10k/powerlevel10k"/' "$zshrc_filepath"
+  if [[ "${machine}" == "Mac" ]]; then
+    # substitue and write to file only works with -i and -e flags in Mac
+    sed -i -e 's/^ZSH_THEME=".*"/ZSH_THEME="powerlevel10k/powerlevel10k"/' "$zshrc_filepath"
+  elif [[ "${machine}" == "Linux" ]]; then
+    sed -i 's/^ZSH_THEME=".*"/ZSH_THEME="powerlevel10k/powerlevel10k"/' "$zshrc_filepath"
+  fi
 
   clone_repo_to_dir "Fish-like Auto-Suggestions" "https://github.com/zsh-users/zsh-autosuggestions" "$zsh_custom/plugins/zsh-autosuggestions"
-  # sed command with -i and -e flags works on Mac
   # Intended purpose: Find and replace plugins value and write changes to .zshrc
   echo "Updating the plugins in .zshrc to use zsh-autosuggestions"
-  sed -i -e 's/^plugins=\(.*\)/plugins=(zsh-autosuggestions)/' "$zshrc_filepath"
+  if [[ "${machine}" == "Mac" ]]; then
+    # sed command with -i and -e flags works on Mac
+    sed -i -e 's/^plugins=\(.*\)/plugins=(zsh-autosuggestions)/' "$zshrc_filepath"
+  elif [[ "${machine}" == "Linux" ]]; then
+    sed -i 's/^plugins=\(.*\)/plugins=(zsh-autosuggestions)/' "$zshrc_filepath"
+  fi
 
   echo "Creating aliases"
   touch "$zsh_custom/aliases.zsh"
