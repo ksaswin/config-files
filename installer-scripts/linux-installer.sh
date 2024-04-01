edaf19bf71788f55f31f24d5978d6ea9622a433a
Aswin-Shaji <aswin2000.ak@gmail.com>
2024-04-01 12:12:28 +0530

    Update linux installer to find package manager for distro

--- installer-scripts/linux-installer.sh
+++ installer-scripts/linux-installer.sh
@@ -1,6 +1,32 @@
 #!/usr/bin/env bash
 
 source ./logs.sh
+source ./helpers.sh
+
+PACKAGE_MANAGER=""
+
+declare -A osInfo;
+osInfo[/etc/redhat-release]=yum
+osInfo[/etc/arch-release]=pacman
+osInfo[/etc/gentoo-release]=emerge
+osInfo[/etc/SuSE-release]=zypp
+osInfo[/etc/debian_version]=apt-get
+osInfo[/etc/alpine-release]=apk
+
+for f in ${!osInfo[@]}
+do
+  if [[ -f $f ]];then
+    PACKAGE_MANAGER=${osInfo[$f]}
+  fi
+done
+
+if [[ $PACKAGE_MANAGER == "" ]]; then
+  log "Could not find any package managers for the distro."
+  exit
+fi
 
 
-log "Inside the Linux installer script..."
+# Saving HOME path, since wildcard characters (~) does not resolve in conditions
+HOME=$(echo $HOME)
+
+# oh_my_zsh $HOME # FIXME: Uncomment when ready
