cae2553037bfdfd7dcb8d282ffaab2cdf1f3d3ad
ksaswin <aswin2000.ak@gmail.com>
2026-06-19 22:57:36 +0530

    Update pbcopy and pbpaste aliases

--- ohmyzsh/arch_aliases.zsh
+++ ohmyzsh/arch_aliases.zsh
@@ -3,14 +3,12 @@ alias yoink="paru -S"
 alias yeet="paru -R"
 
 # NVM usage
-export NVM_DIR="/usr/share/nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+# export NVM_DIR="/usr/share/nvm"
+# [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 
 # TMUX (Setting tmux to use utf-8)
-alias tmux="tmux -u"
+# alias tmux="tmux -u"
 
 # pbcopy & pbpaste in arch
-# NOTE: Depends on the xsel package
-# pacman -S xsel
-alias pbcopy='xsel --clipboard --input'
-alias pbpaste='xsel --clipboard --output'
+alias pbcopy='wl-copy'
+alias pbpaste='wl-paste'
